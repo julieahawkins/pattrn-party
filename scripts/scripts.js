57827639792b3b75af5cda56272e882c413cfead
julie hawkins <julabi@gmail.com>
2018-02-06 20:56:48 -0700

    adds squishy styles and basic mobile tab function skeleton

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,19 @@
-$('li').on('click', changeTab);
+$('.tab').on('click', changeTab);
 
 function changeTab() {
-  $('li').removeClass('active-tab');
+  if ($(window).width() < 550) {
+    changeTabMobile(this);
+  }
+
+  $('.tab').removeClass('active-tab');
   $(this).addClass('active-tab');
 
   $('.tab-content').addClass('none');
   const tab = $(this).text().toLowerCase().replace(' ', '-');
+
   $(`.${tab}`).removeClass('none');
+}
+
+function changeTabMobile(tab) {
+  console.log(tab)
 }
