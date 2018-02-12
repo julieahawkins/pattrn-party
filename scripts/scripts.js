0f0ee303ce0a6ad668e268a8403582ad8d775f4a
julie hawkins <julabi@gmail.com>
2018-02-11 18:01:10 -0700

    fixes accordian tabs ;)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,43 @@
 $('.tab').on('click', changeTab);
+$(window).on('resize', updateTabContent);
+$(window).on('load', updateTabContent);
 
 function changeTab() {
-  if ($(window).width() < 550) {
-    changeTabMobile(this);
+  const tabContentID = $(this).clone().children().remove().end().text().toLowerCase().replace(' ', '-');
+
+  if ($(window).width() < 775 && $(this).hasClass('active-tab')) {
+    $(this).toggleClass('active-tab');
+  } else {
+    $('.tab').removeClass('active-tab');
+    $(this).addClass('active-tab');
+    $('.tab-content').addClass('none');
+    $(`#${tabContentID}`).removeClass('none');
   }
 
-  $('.tab').removeClass('active-tab');
-  $(this).addClass('active-tab');
+  if ($(window).width() < 775) {
+    renderMobile(this, tabContentID);
+  }
+};
 
-  $('.tab-content').addClass('none');
-  const tab = $(this).text().toLowerCase().replace(' ', '-');
+function updateTabContent () {
+  if ($(window).width() < 775) {
+    $('.tab-content-container').appendTo($('.active-tab'));
+  } else {
+    $('.tabs-container').after($('.tab-content-container'));
+    
+    if (!$('.tab').hasClass('active-tab')) {
+      $('.tab').first().addClass('active-tab');
+      $('#tab-1').removeClass('none');
+    }
+  }
+};
 
-  $(`.${tab}`).removeClass('none');
-}
+function renderMobile(tab, tabContentID) {
+  $('.tab-content-container').appendTo($(tab));
 
-function changeTabMobile(tab) {
-  console.log(tab)
-}
+  if ($(tab).hasClass('active-tab')) {
+    $(`#${tabContentID}`).removeClass('none');
+  } else {
+    $(`#${tabContentID}`).addClass('none');
+  }
+};
